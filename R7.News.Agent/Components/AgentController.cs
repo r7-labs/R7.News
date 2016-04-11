d5d5e931af8c0409301de147a98c1168ac7414e0
roman-yagodin <roman.yagodin@gmail.com>
2016-04-11 12:15:47 +0300

    Implement IPortable for Agent module

--- R7.News.Agent/Components/AgentController.cs
+++ R7.News.Agent/Components/AgentController.cs
@@ -21,16 +21,24 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
 using DotNetNuke.Common.Utilities;
+using DotNetNuke.Entities.Content.Taxonomy;
 using DotNetNuke.Entities.Modules;
+using DotNetNuke.Services.Exceptions;
+using DotNetNuke.Services.FileSystem;
+using DotNetNuke.Services.Log.EventLog;
 using DotNetNuke.Services.Search.Entities;
 using R7.News.Data;
 using R7.News.Models;
 
 namespace R7.News.Agent.Components
 {
-    public partial class AgentController : ModuleSearchBase
+    public class AgentController : ModuleSearchBase, IPortable
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="R7.News.Agent.Components.AgentController"/> class.
@@ -71,6 +79,127 @@ namespace R7.News.Agent.Components
 
             return searchDocs;
         }
+
+        #endregion
+
+        #region IPortable implementation
+
+        public string ExportModule (int ModuleID)
+        {
+            XmlWriter xmlWriter = null;
+
+            var moduleController = new ModuleController ();
+            var module = moduleController.GetModule (ModuleID);
+
+            try {
+                var xml = new StringBuilder ();
+                var xmlSerializer = new XmlSerializer (typeof (List<XmlNewsEntryInfo>));
+
+                // get news entries
+                var newsEntries = NewsRepository.Instance.GetNewsEntriesByAgent (module.ModuleID, module.PortalID)
+                    .Select (ne => new XmlNewsEntryInfo (ne)).ToList ();
+
+                if (newsEntries != null) {
+                    var xmlSettings = new XmlWriterSettings ();
+                    using (xmlWriter = XmlWriter.Create (xml, xmlSettings)) {
+                        xmlSerializer.Serialize (xmlWriter, newsEntries);
+                        xmlWriter.Close ();
+                    }
+                }
+
+                return xml.ToString ();
+            }
+            catch (Exception ex) {
+                var logController = new EventLogController ();
+
+                var logInfo = new LogInfo {
+                    Exception = new ExceptionInfo (ex),
+                    LogTypeKey = EventLogController.EventLogType.ADMIN_ALERT.ToString (),
+                    LogUserID = -1, // superuser
+                    LogPortalID = module.PortalID
+                };
+
+                logInfo.AddProperty ("R7.News.Agent", "Cannot export module");
+                logController.AddLog (logInfo);
+            }
+            finally {
+                if (xmlWriter != null) {
+                    xmlWriter.Close ();
+                }
+            }
+
+            return string.Empty;
+        }
+
+        public void ImportModule (int ModuleID, string Content, string Version, int UserID)
+        {
+            XmlReader xmlReader = null;
+
+            var moduleController = new ModuleController ();
+            var module = moduleController.GetModule (ModuleID);
+
+            try {
+                var xmlSerializer = new XmlSerializer (typeof (List<XmlNewsEntryInfo>));
+                using (xmlReader = XmlReader.Create (new StringReader (Content))) {
+                    // deserialize
+                    var xmlNewsEntries = (List<XmlNewsEntryInfo>) xmlSerializer.Deserialize (xmlReader);
+                    xmlReader.Close ();
+
+                    var termController = new TermController ();
+
+                    // add news entries
+                    foreach (var xmlNewsEntry in xmlNewsEntries) {
+                        // get news entry and reset ids
+                        var newsEntry = xmlNewsEntry.GetNewsEntryInfo ();
+                        newsEntry.EntryId = 0;
+                        newsEntry.AgentModuleId = ModuleID;
+                        newsEntry.ContentItemId = 0;
+                        newsEntry.PortalId = module.PortalID;
+
+                        // get terms by ids
+                        var terms = new List<Term> ();
+                        foreach (var termId in xmlNewsEntry.TermIds) {
+                            var term = termController.GetTerm (termId);
+                            if (term != null) {
+                                terms.Add (term);
+                            }
+                        }
+
+                        // get images by ids
+                        var images = new List<IFileInfo> ();
+                        foreach (var imageFileId in xmlNewsEntry.ImageFileIds) {
+                            var image = FileManager.Instance.GetFile (imageFileId);
+                            if (image != null) {
+                                images.Add (image);
+                            }
+                        }
+
+                        // add news entry
+                        NewsRepository.Instance.AddNewsEntry (newsEntry, terms, images,
+                            ModuleID, module.TabID);
+                    }
+                }
+            }
+            catch (Exception ex) {
+                var logController = new EventLogController ();
+
+                var logInfo = new LogInfo {
+                    Exception = new ExceptionInfo (ex),
+                    LogTypeKey = EventLogController.EventLogType.ADMIN_ALERT.ToString (),
+                    LogUserID = -1, // superuser
+                    LogPortalID = module.PortalID
+                };
+
+                logInfo.AddProperty ("R7.News.Agent", "Cannot import module");
+                logController.AddLog (logInfo);
+            }
+            finally {
+                if (xmlReader != null) {
+                    xmlReader.Close ();
+                }
+            }
+        }
+
         #endregion
     }
 }
---
+++ R7.News/Data/XmlNewsEntryInfo.cs
@@ -0,0 +1,233 @@
+﻿//
+//  XmlNewsEntryInfo.cs
+//
+//  Author:
+//       Roman M. Yagodin <roman.yagodin@gmail.com>
+//
+//  Copyright (c) 2016 Roman M. Yagodin
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+using DotNetNuke.Entities.Content;
+using DotNetNuke.Entities.Modules;
+using R7.News.Models;
+
+namespace R7.News.Data
+{
+    /// <summary>
+    /// XML serialization adapter for NewsEntryInfo
+    /// </summary>
+    [Serializable]
+    public class XmlNewsEntryInfo: INewsEntry
+    {
+        protected NewsEntryInfo @This;
+
+        public XmlNewsEntryInfo ()
+        {
+            @This = new NewsEntryInfo ();
+        }
+
+        public XmlNewsEntryInfo (NewsEntryInfo newsEntry)
+        {
+            @This = newsEntry;
+        }
+
+        public NewsEntryInfo GetNewsEntryInfo ()
+        {
+            return @This;
+        }
+
+        #region INewsEntry implementation
+
+        // then deserializing, new entry should be created
+        public int EntryId
+        {
+            get { return @This.EntryId; }
+            set { @This.EntryId = value; }
+        }
+
+        // then deserializing, PortalId should be set to target portal Id
+        public int PortalId
+        {
+            get { return @This.PortalId; }
+            set { @This.PortalId = value; }
+        }
+
+        // then deserializing, new content item should be created
+        public int ContentItemId
+        {
+            get { return @This.ContentItemId; }
+            set { @This.ContentItemId = value; }
+        }
+
+        // then deserializing, AgentModuleId should be set to agent's module id
+        public int? AgentModuleId
+        {
+            get { return @This.AgentModuleId; }
+            set { @This.AgentModuleId = value; }
+        }
+
+        public string Url
+        {
+            get { return @This.Url; }
+            set { @This.Url = value; }
+        }
+
+        public DateTime? StartDate
+        {
+            get { return @This.StartDate; }
+            set { @This.StartDate = value; }
+        }
+
+        public DateTime? EndDate
+        {
+            get { return @This.EndDate; }
+            set { @This.EndDate = value; }
+        }
+
+        public DateTime? ThresholdDate
+        {
+            get { return @This.ThresholdDate; }
+            set { @This.ThresholdDate = value; }
+        }
+
+        public DateTime? DueDate
+        {
+            get { return @This.DueDate; }
+            set { @This.DueDate = value; }
+        }
+
+        public string Title
+        {
+            get { return @This.Title; }
+            set { @This.Title = value; }
+        }
+
+        public string Description
+        {
+            get { return @This.Description; }
+            set { @This.Description = value; }
+        }
+
+        public int SortIndex
+        {
+            get { return @This.SortIndex; }
+            set { @This.SortIndex = value; }
+        }
+
+        public bool IsSticky
+        {
+            get { return @This.IsSticky; }
+            set { @This.IsSticky = value; }
+        }
+
+        public int ThematicWeight
+        {
+            get { return @This.ThematicWeight; }
+            set { @This.ThematicWeight = value; }
+        }
+
+        public int StructuralWeight
+        {
+            get { return @This.StructuralWeight; }
+            set { @This.StructuralWeight = value; }
+        }
+       
+        [XmlIgnore]
+        public ContentItem ContentItem
+        {
+            get { return @This.ContentItem; }
+            set { @This.ContentItem = value; }
+        }
+
+        [XmlIgnore]
+        public ModuleInfo AgentModule
+        {
+            get { return @This.AgentModule; }
+            set { @This.AgentModule = value; }
+        }
+
+        [XmlIgnore]
+        public ICollection<INewsEntry> Group
+        {
+            get { return @This.Group; }
+            set { @This.Group = value; }
+        }
+
+        #endregion
+
+        // Terms and image files should be serialized as their ids.
+        // Then deserializing, terms and files with those ids should exist -
+        // if some are not, we should skip those items.
+
+        private List<int> termIds;
+
+        private List<int> imageFileIds;
+
+        /// <summary>
+        /// Gets the news entry terms.
+        /// </summary>
+        /// <value>The terms.</value>
+        public List<int> TermIds
+        {
+            get 
+            {
+                if (termIds == null) {
+                    if (ContentItem != null) {
+                        // on export
+                        termIds = ContentItem.Terms.Select (t => t.TermId).ToList ();
+                    }
+                    else {
+                        // on import
+                        termIds = new List<int> ();
+                    }
+                }
+
+                return termIds;
+            }
+           
+            set { termIds = value; }
+        }
+
+        /// <summary>
+        /// Gets the news entry image file ids.
+        /// </summary>
+        /// <value>The terms.</value>
+        public List<int> ImageFileIds
+        {
+            get 
+            {
+                if (imageFileIds == null) {
+                    if (ContentItem != null) {
+                        // on export
+                        imageFileIds = ContentItem.Images.Select (i => i.FileId).ToList ();
+                    }
+                    else {
+                        // on import
+                        imageFileIds = new List<int> ();
+                    }
+                }
+
+                return imageFileIds;
+            }
+
+            set { imageFileIds = value; }
+        }
+    }
+}
+
