0de2b5555e1f4a279e89262b11d8fbe323ea4569
roman-yagodin <roman.yagodin@gmail.com>
2017-04-06 10:57:41 +0300

    Remove obsolete code

--- R7.News/Modules/NewsModuleBase.cs
+++ R7.News/Modules/NewsModuleBase.cs
@@ -19,17 +19,11 @@
 //  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-using System.Web.UI.WebControls;
 using DotNetNuke.Entities.Icons;
 using DotNetNuke.Entities.Modules;
 using DotNetNuke.Entities.Modules.Actions;
 using DotNetNuke.Security;
-using DotNetNuke.Services.Log.EventLog;
 using R7.DotNetNuke.Extensions.Modules;
-using R7.News.Components;
-using R7.News.Data;
-using R7.News.Providers.DiscussProviders;
-using System.Linq;
 
 namespace R7.News.Modules
 {
@@ -59,26 +53,5 @@ namespace R7.News.Modules
         }
 
         #endregion
-
-        protected void linkDiscuss_Command (object sender, CommandEventArgs e)
-        {
-            var entryId = int.Parse ((string)e.CommandArgument);
-            var newsEntry = NewsRepository.Instance.GetNewsEntry (entryId, PortalId);
-            var discussProvider = NewsConfig.Instance.GetDiscussProviders ().FirstOrDefault ();
-            if (newsEntry != null && discussProvider != null) {
-                var discussId = discussProvider.Discuss (newsEntry, PortalId, UserId);
-
-                if (discussId > 0) {
-                    Response.Redirect (discussProvider.GetDiscussUrl (discussId));
-                }
-                else {
-                    var log = new LogInfo ();
-                    log.LogTypeKey = EventLogController.EventLogType.ADMIN_ALERT.ToString ();
-                    log.LogPortalID = PortalId;
-                    log.AddProperty ("Message", "Cannot create discussion for a news entry");
-                    EventLogController.Instance.AddLog (log);
-                }
-            }
-        }
     }
 }
