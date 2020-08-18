7b44cc2236056d34b45e6c158be80baf1034b43e
roman-yagodin <roman.yagodin@gmail.com>
2020-08-18 17:27:12 +0400

    Generate DNN manifest, use shared SolutionInfo.cs

---
+++ R7.News/Properties/SolutionInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany ("R7.Labs")]
+[assembly: AssemblyProduct ("R7.News")]
+[assembly: AssemblyCopyright ("Roman M. Yagodin")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyVersion ("1.7.1")]
+[assembly: AssemblyInformationalVersion ("1.7.1")]
