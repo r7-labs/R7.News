06c7d3313220aa07a760032a8fdbf046d52671ea
roman-yagodin <roman.yagodin@gmail.com>
2020-10-06 16:23:03 +0400

    Temporary workaround for GH-113

--- R7.News/Feeds/AtomFeed.cs
+++ R7.News/Feeds/AtomFeed.cs
@@ -1,25 +1,4 @@
-﻿//
-//  AtomFeed.cs
-//
-//  Author:
-//       Roman M. Yagodin <roman.yagodin@gmail.com>
-//
-//  Copyright (c) 2019 Roman M. Yagodin
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Affero General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Affero General Public License for more details.
-//
-//  You should have received a copy of the GNU Affero General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -36,7 +15,6 @@ namespace R7.News.Feeds
     {
         string IsoDateTime (DateTime datetime) => datetime.ToUniversalTime ().ToString ("s") + "Z";
 
-        // TODO: Move to the base library?
         string Base64ToCanonicalForm (string base64String) => base64String.Replace ("%3d", "%3D");
 
         public void Render (XmlWriter writer, IEnumerable<NewsEntry> newsEntries, ModuleInfo module,
@@ -88,6 +66,10 @@ namespace R7.News.Feeds
                 writer.WriteAttributeString ("type", "html");
 
                 var htmlContent = HttpUtility.HtmlDecode (n.Description);
+
+                // FIXME: Temporary workaround for GH-113
+                htmlContent = htmlContent.Replace ("&mdash;", "&ndash;");
+
                 if (withImages && n.ContentItem.Images.Count > 0) {
                     var imageUrl = n.GetRawImageUrl ();
                     if (!string.IsNullOrEmpty (imageUrl)) {
